d541c31dc11157d78c71ef899c8c0b6ad2373e42
Denver <dnaicker@users.noreply.github.com>
2023-02-23 07:05:22 +0200

    fix for datasource location

--- src/api/credentials/trinsicV2VerifiableCredentials.ts
+++ src/api/credentials/trinsicV2VerifiableCredentials.ts
@@ -28,9 +28,9 @@ import {
   ServerRoute,
 } from "@hapi/hapi";
 import { v4 as uuid } from "uuid";
-import { AppDataSource } from "../../../data-source"
-import { CredentialTemp } from "../../../entity/CredentialTemp"
-import { Verification } from "../../../entity/Verification";
+import { AppDataSource } from "../../database/data-source";
+import { CredentialTemp } from "../../database/entity/CredentialTemp"
+import { Verification } from "../../database/entity/Verification";
 
 const trinsic = new TrinsicService();
 
